ca3dd35989080c91180f237aabfeeb2d2941f0c8
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-09-30 20:58:41 +0530

    fix

--- src/app/components/NewTestimonials.tsx
+++ src/app/components/NewTestimonials.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import TestimonialCard from "../components/TestimonialCard";
 import Button from "./Button";
 
@@ -61,7 +60,6 @@ const NewTestimonials = () => {
                     ? item.image
                     : "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 }
-                rating={5}
               />
             ))}
           </div>
