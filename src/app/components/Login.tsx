1a5ce3e1eeeb42810eb324c43885070b5bbe7a27
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-09-30 21:16:36 +0530

    fix

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const Login: React.FC = () => {
